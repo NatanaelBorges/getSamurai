45f3c18387d572adac23da42af170585277b0fd8
Natanael Borges <natanael.borges@luizalabs.com>
2022-06-22 14:41:28 -0300

    refactor: refactor the user entity by adding more fields
    feat: create the initial database migration

--- sources/server/src/database/data-source.ts
+++ sources/server/src/database/data-source.ts
@@ -10,10 +10,19 @@ export const AppDataSource = new DataSource({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     synchronize: false,
+    migrationsRun: true,
     logging: false,
     entities: [
-        'src/entity/*{.ts,.js}'
+        'src/database/entities/*{.ts,.js}'
     ],
-    migrations: [],
+    migrations: [
+      'src/database/migrations/*{.ts,.js}'
+    ],
     subscribers: [],
-})
+});
+
+AppDataSource.initialize()
+    .then(() => {
+      console.log('Database connected')
+    })
+    .catch((error) => console.error('Database not connected', error));
